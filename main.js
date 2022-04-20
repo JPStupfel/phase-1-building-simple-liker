2a7c90a37ba52d2b1611f6a922e001c723c3c98f
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-04-20 18:06:44 -0400

    commit tree ca7a59be3a8f68032120009bc7f2e0180e7558d4

--- main.js
+++ main.js
@@ -3,8 +3,23 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const oops = document.querySelector('#modal')
+const like = document.querySelectorAll('.like-glyph')
+console.log(typeof(like))
+for (let element of like) {element.addEventListener('click',()=>handleclick(element))}
 
+function addHeart(comp){comp.classList.add('activated-heart');comp.textContent = FULL_HEART}
+function removeHeart(comp){comp.classList.remove('activated-heart'); comp.textContent = EMPTY_HEART}
 
+function removeHidden(){ oops.classList.remove("hidden") }
+
+function addHidden(){oops.classList.add('hidden')}
+
+function handleclick(instance){
+  if (instance.className === 'like-glyph') {console.log(instance.className);return mimicServerCall().then(res=>{
+    console.log(res);addHeart(instance);addHidden()}).catch(error=>{console.log(error);oops.textContent=error;removeHeart(instance);removeHidden();setTimeout(addHidden,3000)})}
+  else {removeHeart(instance)}
+}
 
 
 //------------------------------------------------------------------------------
